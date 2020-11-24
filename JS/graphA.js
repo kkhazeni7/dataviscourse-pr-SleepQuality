b1ec310f13a45603c84b3a65029f6002d6ac0f05
kkhazeni7 <u1135484@utah.edu>
2020-11-24 11:14:37 -0700

    Added Data and Mouse over to the path to change color

--- JS/graphA.js
+++ JS/graphA.js
@@ -42,6 +42,8 @@ class Graph
 
         for(let i = 0; i < 8; i++)
         {
+
+            
             if(i < 4)
             {
             var yLoc = (i + 1) * 200;
@@ -52,45 +54,63 @@ class Graph
                                 .attr("height" ,250 )
                                 .attr("x",20)
                                 .attr("y",yLoc - 100)
+          
                                 
-                                
-
-           
             
             //var dataObject = new Date();
+            //var date holds all the dates of the persons data, where as dateArr has the
+            //first date, the middle date, and the last date for tick purposes on the x
+            //axis for each graph
             var date = currPerson.map(d => d.dateOfSleep)
+            var dateArr = [date[0], date[Math.floor(date.length /2)], date[date.length - 1]]
+
+            //Quality will be the overall score of the persons sleep
+            //the min_max var will hold the range from the min and max of those scores
             var quality = currPerson.map(d => d.overall_score)
-            console.log(quality)
             var min_max = d3.extent(quality)
-            //console.log(min_max)
-            //console.log(Date.parse(date[0]))
-            //console.log(Date.parse(date[10]))
+          
             this.xScale = d3.scaleLinear()
                         .domain([new Date(Date.parse(date[0])), new Date(Date.parse(date[date.length - 1]))])
-                        .range([50,200])
+                        .range([50,250])
             this.yScale = d3.scaleLinear()
                         .domain([0,100])
                         .range([yLoc -100, yLoc - 190])
-           that.svgChart.append("text")
+            that.svgChart.append("text")
                         .attr("x" , yLoc - 875)
                         .attr("y" , 25)
                         .style("stroke" , "black")
                         .style("font-size", "9pt")
-                        .attr("transform" , "rotate(270)")
-                        //.attr("transform" , )
-                        
+                        .attr("transform" , "rotate(270)")         
                         .text("Quality")
+            that.svgChart.append("text")
+                        .attr("x" , 130)
+                        .attr("y" , yLoc-125)
+                        .style("stroke" , "grey")
+                        .style("font-size", "12pt")
+                        .style("stroke-width" , .7)
+                        .text("Person " + (i+1))
 
            for(let j = 0; j < yTicks.length; j++)
             {
                 that.svgChart.append("text")
                               .attr("x" ,45)
                               .attr("y", that.yScale(yTicks[j]))
-                              .style("stroke" , "red")
+                              .style("stroke" , "gray")
                               .style("font-size", "9pt")
                               .style("text-anchor" , "end")
                               .text(yTicks[j])
-                }
+            }
+            for(let j = 0; j < dateArr.length; j++)
+            {
+                that.svgChart.append("text")
+                             .attr("x", 50 + (75*j))
+                             .attr("y" , yLoc - 80)
+                             .style("stroke" , "gray")
+                             .style("font-size", "9pt")
+                             .style("text-anchor" , "start")
+                             .text(dateArr[j])
+                             
+            }
             let lineX = that.view.select(".p_chart"+(i+1)).append("line")
                          .attr("x1" , 50)
                          .attr("y1" ,yLoc-100)
@@ -106,6 +126,27 @@ class Graph
                          .attr("y2" , yLoc - 200)
                          .style("stroke-width", 2)
                          .style("stroke" , "black")
+            
+            let path = that.view.select(".p_chart"+(i+1)).append("path")
+                                    .datum(currPerson)
+                                    .attr("fill" , "none")
+                                    .attr("stroke" , "steelblue")
+                                    .attr("stroke-width" , 1.5)
+                                    .attr("d" , d3.line()
+                                        .x(d => that.xScale(new Date(Date.parse(d.dateOfSleep))))
+                                        .y(d => that.yScale(d.overall_score))
+                                    )
+                                    .on("mouseover", function(d)
+                                    {
+                                        that.view.select(".p_chart"+(i+1)).select("path")
+                                        .attr("stroke" , "red")
+                                    })
+                                    .on("mouseout", function(d)
+                                    {
+                                        that.view.select(".p_chart"+(i+1)).select("path")
+                                        .attr("stroke" , "steelblue")
+                                    })
+           
             }
             if(i >= 4)
             {
@@ -118,37 +159,56 @@ class Graph
                                 .attr("x", 300)
                                 .attr("y",yLoc - 100)
                                 
+                                
 
             
             //var dataObject = new Date();
             var date = currPerson.map(d => d.dateOfSleep)
+            var dateArr = [date[0], date[Math.floor(date.length/2)], date[date.length - 1]]
+
             var quality = currPerson.map(d => d.overall_score)
-            console.log(quality)
             var min_max = d3.extent(quality)
-            //console.log(min_max)
-            //console.log(Date.parse(date[0]))
-            //console.log(Date.parse(date[10]))
+          
             this.xScale = d3.scaleLinear()
                         .domain([new Date(Date.parse(date[0])), new Date(Date.parse(date[date.length - 1]))])
-                        .range([50,200])
+                        .range([300,500])
             this.yScale = d3.scaleLinear()
                         .domain([0,100])
                         .range([yLoc -100, yLoc - 190])
+            that.svgChart.append("text")
+                        .attr("x" , 380)
+                        .attr("y" , yLoc-125)
+                        .style("stroke" , "grey")
+                        .style("font-size", "12pt")    
+                        .style("stroke-width" , .7)  
+                        .text("Person " + (i+1))
             for(let j = 0; j < yTicks.length; j++)
             {
                 that.svgChart.append("text")
                               .attr("x" ,295)
                               .attr("y", that.yScale(yTicks[j]))
-                               .style("stroke" , "red")
-                                .style("font-size", "9pt")
-                                .style("text-anchor" , "end")
-                                .text(yTicks[j])
-                            }
+                              .style("stroke" , "gray")
+                              .style("font-size", "9pt")
+                              .style("text-anchor" , "end")
+                              .text(yTicks[j])
+            }
+            for(let j = 0; j < dateArr.length; j++)
+            {
+                that.svgChart.append("text")
+                             .attr("x", 300 + (75*j))
+                             .attr("y" , yLoc - 80)
+                             .style("stroke" , "gray")
+                             .style("font-size", "9pt")
+                             .style("text-anchor" , "start")
+                             .text(dateArr[j])
+                             
+            }
+
             
             let lineX = that.view.select(".p_chart"+(i+1)).append("line")
                          .attr("x1" , 300)
                          .attr("y1" ,yLoc-100)
-                         .attr("x2" , 550)
+                         .attr("x2" , 500)
                          .attr("y2" , yLoc-100)
                          .style("stroke-width", 2)
                          .style("stroke" , "black")
@@ -160,6 +220,27 @@ class Graph
                          .attr("y2" , yLoc - 200)
                          .style("stroke-width", 2)
                          .style("stroke" , "black")
+            console.log("about to draw path")
+            let path = that.view.select(".p_chart"+(i+1)).append("path")
+                                    .datum(currPerson)
+                                    .attr("fill" , "none")
+                                    .attr("stroke" , "steelblue")
+                                    .attr("stroke-width" , 1.5)
+                                    .attr("d" , d3.line()
+                                        .x(d => that.xScale(new Date(Date.parse(d.dateOfSleep))))
+                                        .y(d => that.yScale(d.overall_score))
+                                    )
+                                    .on("mouseover", function(d)
+                                    {
+                                        that.view.select(".p_chart"+(i+1)).select("path")
+                                        .attr("stroke" , "red")
+                                    })
+                                    .on("mouseout", function(d)
+                                    {
+                                        that.view.select(".p_chart"+(i+1)).select("path")
+                                        .attr("stroke" , "steelblue")
+                                    })
+                                        
             }
            
             //that.view.exit()
